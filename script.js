e1f3b8f7b4bd84f7a4f149acd2893614720320b2
hledezma03 <hledezma910@gmail.com>
2024-03-16 16:25:29 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 function getComputerChoice() {
-    let cpuChoice = Math.floor(Math.random()*3) + 1;
-    if (cpuChoice === 1) {
-        cpuChoice = 'Rock';
-    } else if (cpuChoice === 2) {
-        cpuChoice = 'Paper';
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    if (computerChoice === 1) {
+        computerChoice = 'rock';
+    } else if (computerChoice === 2) {
+        computerChoice = 'paper';
     } else {
-        cpuChoice = 'Scissors'
+        computerChoice = 'scissors'
     }
-    return cpuChoice
-}
+    return computerChoice;
+};
 
-console.log(getComputerChoice())
+function playRound(playerChoice,computerChoice) {
+    if (playerChoice == computerChoice) {
+        console.log('Draw!')
+    } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        console.log('You lose! Paper beats Rock')
+    } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        console.log('You win! Rock beats Scissors')
+    } else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        console.log('You win! Paper beats Rock')
+    }else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        console.log('You lose! Scissors beats Paper')
+    }else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        console.log('You lose! Rock beats Scissors')
+    }else {
+        console.log('You win! Scissors beats Paper')
+    }
+};
+
+let playerChoice = 'scissors';
+let computerChoice = getComputerChoice();
+
+playRound(playerChoice,computerChoice);
